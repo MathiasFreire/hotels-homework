30caa9a2a2841724d4b06fd21bf7dac12181a7f5
Michael <mddobrykh@gmail.com>
2023-03-03 23:40:56 +0300

    upd: div task2

--- task2.js
+++ task2.js
@@ -33,7 +33,7 @@ export function sum(a, b) {
 
 export function diff(a, b) {
 
-        if (a[0] === "-" && b[0] !== "-") {
+    if (a[0] === "-" && b[0] !== "-") {
         return "-" + sum(a.slice(1), b);
     }
 
@@ -92,29 +92,29 @@ export function diff(a, b) {
     return sign + res.slice(counter);
 }
 
+function miniMul(a, b) {
+    let res = "";
+    b = +b;
+    let shift = 0;
+    for (let i = a.length - 1; i >= 0; i--) {
+        let curMul = +a[i] * b + shift;
+
+        if (i === 0) {
+            res = curMul + res;
+            break;
+        }
+
+        res = curMul % 10 + res;
+        shift = Math.floor(curMul / 10);
+    }
+    return res;
+}
+
 export function mul(a, b) {
     if (a[0] < b[0] && a.length <= b.length) {
         let buff = a;
         a = b;
         b = buff;
-    }
-
-    function miniMul(a, b) {
-        let res = "";
-        b = +b;
-        let shift = 0;
-        for (let i = a.length - 1; i >= 0; i--) {
-            let curMul = +a[i] * b + shift;
-
-            if (i === 0) {
-                res = curMul + res;
-                break;
-            }
-
-            res = curMul % 10 + res;
-            shift = Math.floor(curMul / 10);
-        }
-        return res;
     }
 
     let res = "";
@@ -128,27 +128,68 @@ export function mul(a, b) {
 }
 
 export function div(a, b) {
-    let counter = "";
-
-    while (diff(a, b) >= 0) {
-        a = diff(a, b);
-        counter = sum(counter, "1");
-    }
 
     function rounding(num1, num2) {
         let difference = Math.abs(num1.length - num2.length);
+        if (difference > 6) return "00000..."
 
-        switch (difference) {
-            case 0:
-                return "" + (((+num1[0] / +num2[0]).toString().at(2)) || "") + "...";
-            case 1:
-                return "0" + (((+num1[0] / +num2[0]).toString().at(3)) || "") + "...";
-            case 2:
-                return "00" + (((+num1[0] / +num2[0]).toString().at(4)) || "") + "...";
-            default:
-                return "000...";
+        let round1 = num1.slice(0, 6);
+        let round2 = num2.slice(0, 6 + difference);
+
+        if (round1 === round2) return "99999..."
+
+        return (+round1 / +round2).toString().slice(2, 7) + "...";
+    }
+
+
+    if (a.length < b.length || (a.length === b.length && a < b)) {
+        return 0 + "." + rounding(a, b);
+    }
+
+    let res = "";
+    let curDiff = "";
+    let coefficient = 1;
+
+    let curStr = a.slice(0, b.length);
+
+    while (true) {
+        curDiff = diff(curStr, miniMul(b, coefficient.toString()));
+
+        if (curStr.length < b.length || (curStr.length === b.length && curStr < b)) {
+            curStr += a[curStr.length];
+            continue;
+        }
+
+        if (curDiff.length < b.length ||
+            (curDiff.length === b.length && curDiff < b)) {
+            res += coefficient;
+            break;
+        } else {
+            coefficient++;
         }
     }
 
-    return (counter || 0) + "." + rounding(diff(sum(a, b), b), b);
+    for (let i = curStr.length; i < a.length; i++) {
+        coefficient = 1;
+        curStr = curDiff + a[i];
+        if (curStr.length < b.length ||
+            (curStr.length === b.length && curStr < b)) {
+            res += "0";
+            continue;
+        }
+        curDiff = diff(curStr, miniMul(b, coefficient.toString()));
+        while (curDiff.length > b.length || (curDiff.length === b.length && curDiff >= b)) {
+            coefficient++;
+            curDiff = diff(curStr, miniMul(b, coefficient.toString()));
+        }
+        if (diff(curStr, miniMul(b, coefficient.toString())) === "0") {
+            curDiff = 0;
+        }
+        res += coefficient;
+    }
+
+    if (curDiff === "") return res;
+
+    return res + "." + rounding(curDiff, b);
+
 }
